29077ceddb852ded6533d90308f7aee156ff085b
MaxCoppa <maxime.coppa@gmail.com>
2026-02-19 21:44:57 +0000

    test moirai

---
+++ moirai-classification/README.md
@@ -0,0 +1,3 @@
+uv sync 
+
+https://www.youtube.com/watch?v=zgSQr0d5EVg&t=69s
---
+++ moirai-classification/main.py
@@ -0,0 +1,7 @@
+# %%
+from uni2ts.eval_util.data import get_gluonts_test_dataset
+from uni2ts.eval_util.plot import plot_next_multi
+from uni2ts.model.moirai import MoiraiForecast, MoiraiModule
+# %%
+model = MoiraiModule.from_pretrained("Salesforce/moirai-1.1-R-small")
+
