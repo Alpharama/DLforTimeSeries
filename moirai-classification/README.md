aed84367fa592d39236c253f3c6f5580e5c58481
MaxCoppa <maxime.coppa@gmail.com>
2026-02-22 10:02:07 +0000

    clean notebook

--- moirai-classification/README.md
+++ moirai-classification/README.md
@@ -1,3 +1,44 @@
+
+To init
 uv sync 
 
-https://www.youtube.com/watch?v=zgSQr0d5EVg&t=69s
+Put in the good env
+.venv 
+
+To test well define 
+uv run main.py
+
+What do I do : 
+
+
+https://medium.com/data-science-collective/zero-shot-forecast-with-moirai-moe-f81c764bf0e2
+https://github.com/anamabo/medium-blogs/tree/main/moirai-moe
+
+https://github.com/SalesforceAIResearch/uni2ts
+https://github.com/SalesforceAIResearch/uni2ts/tree/main/example
+https://huggingface.co/Salesforce/moirai-1.0-R-large
+
+https://github.com/SalesforceAIResearch/uni2ts/tree/main/src/uni2ts/model/moirai
+- forecast.py
+- module.py
+
+I implement MoiraiEncoder in encoder.py using MoiraiModule and MoiraiForecaster from https://github.com/SalesforceAIResearch/uni2ts/tree/main/src/uni2ts/model/moirai 
+- forecast.py
+- module.py
+
+
+In the initial code : 
+1. Apply scaling to observations
+2. Project from observations to representations
+3. Replace prediction window with learnable mask
+4. Apply transformer layers
+5. Project from representations to distribution parameters
+6. Return distribution object
+
+I drop 5. 6., 
+
+Moreover model take as input processed inputs. So i keep the _convert method from MoiraiForecaster.   
+
+
+TO test : 
+moirai.ipynb
--- moirai-classification/main.py
+++ moirai-classification/main.py
@@ -1,62 +1,12 @@
 # %%
-from uni2ts.eval_util.data import get_gluonts_test_dataset
-from uni2ts.eval_util.plot import plot_next_multi
-from uni2ts.model.moirai import MoiraiForecast, MoiraiModule
+from tslearn.datasets import UCR_UEA_datasets
+from uni2ts.model.moirai import MoiraiModule
+
+# %%
+ds = UCR_UEA_datasets()
+X_train, y_train, X_test, y_test = ds.load_dataset("LSST")
 
 # %%
 module = MoiraiModule.from_pretrained("Salesforce/moirai-1.1-R-small")
 
 # %%
-module
-# %%
-
-'''
-Module forward
-def forward(
-    self,
-    target: Float[torch.Tensor, "*batch seq_len max_patch"],
-    observed_mask: Bool[torch.Tensor, "*batch seq_len max_patch"],
-    sample_id: Int[torch.Tensor, "*batch seq_len"],
-    time_id: Int[torch.Tensor, "*batch seq_len"],
-    variate_id: Int[torch.Tensor, "*batch seq_len"],
-    prediction_mask: Bool[torch.Tensor, "*batch seq_len"],
-    patch_size: Int[torch.Tensor, "*batch seq_len"],
-) -> Distribution:
-    """
-    Defines the forward pass of MoiraiModule.
-    This method expects processed inputs.
-
-    1. Apply scaling to observations
-    2. Project from observations to representations
-    3. Replace prediction window with learnable mask
-    4. Apply transformer layers
-    5. Project from representations to distribution parameters
-    6. Return distribution object
-
-    :param target: input data
-    :param observed_mask: binary mask for missing values, 1 if observed, 0 otherwise
-    :param sample_id: indices indicating the sample index (for packing)
-    :param time_id: indices indicating the time index
-    :param variate_id: indices indicating the variate index
-    :param prediction_mask: binary mask for prediction horizon, 1 if part of the horizon, 0 otherwise
-    :param patch_size: patch size for each token
-    :return: predictive distribution
-    """
-    loc, scale = self.scaler(
-        target,
-        observed_mask * ~prediction_mask.unsqueeze(-1),
-        sample_id,
-        variate_id,
-    ) # Scale the observation
-    scaled_target = (target - loc) / scale
-    reprs = self.in_proj(scaled_target, patch_size) # Project
-    masked_reprs = mask_fill(reprs, prediction_mask, self.mask_encoding.weight) # Mask 
-    reprs = self.encoder(
-        masked_reprs,
-        packed_attention_mask(sample_id),
-        time_id=time_id,
-        var_id=variate_id,
-    )
-
-    return reprs
-'''
